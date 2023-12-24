7e6ee7360aaf966b3b8ae81af86bfe4bcb247ec3
Paul Brown <paulbrown389@gmail.com>
2023-12-24 18:35:43 -0500

    Filter

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solution here
+function findMatching(drivers, name) {
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+  }
+  function fuzzyMatch(drivers, letters) {
+    const matchedDrivers = [];
+  
+    drivers.forEach(driver => {
+      if (driver.startsWith(letters)) {
+        matchedDrivers.push(driver);
+      }
+    });
+  
+    return matchedDrivers;
+  }
+  function matchName(drivers, name) {
+    const matchedDrivers = [];
+  
+    drivers.forEach(driver => {
+      if (driver.name === name) {
+        matchedDrivers.push(driver);
+      }
+    });
+  
+    return matchedDrivers;
+  }
